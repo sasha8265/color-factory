3e71bd28215c1b27dadd6b142e9c7907e2fc1a28
Sasha Czerniawski <sashacandoit@gmail.com>
2022-12-28 07:13:21 -0500

    added color detail route

--- src/ColorDetail.js
+++ src/ColorDetail.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link, Navigate, useParams } from "react-router-dom";
+
 
 const ColorDetail = () => {
+    const { color } = useParams();
 
+    if (!color) return <Navigate to="/colors" replace />
+    return (
+        <div className="ColorDetail">
+            <Link to="/colors">Back</Link>
+            <h1 className="ColorDetail-title">{color.color}</h1>
+        </div>
+    )
 };
 
 export default ColorDetail;
--- src/ColorRoutes.js
+++ src/ColorRoutes.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
+import ColorList from "./ColorsList"
+import ColorForm from "./ColorForm";
+import ColorDetail from "./ColorDetail"
 
 const ColorRoutes = () => {
-
+    return (
+        <Routes>
+            <Route exact path="/colors" element={<ColorList />}></Route>
+            <Route exact path="/colors/new" element={<ColorForm />}></Route>
+            <Route path="/colors/:color" element={<ColorDetail />}></Route>
+            <Route path="/" element={<Navigate to="/colors" replace />} />
+        </Routes>
+    )
 };
 
 export default ColorRoutes;
